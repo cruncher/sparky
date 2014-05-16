3f2098dabcd270cf8c8aec9162375e206f6125e0
stephband <stephband@googlemail.com>
2014-05-16 16:57:49 +0200

    Fixes function date name, which was getting lost in the minifier.

--- src/sparky.filters.js
+++ src/sparky.filters.js
@@ -103,7 +103,7 @@
 			
 			var rletter = /([a-zA-Z])/g;
 			
-			return function date(format) {
+			return function formatDate(format) {
 				var date = this instanceof Date ? this : new Date(this) ;
 				
 				return format.replace(rletter, function($0, $1) {
@@ -187,8 +187,10 @@
 		
 		//phone2numeric
 
-		pluralize: function() {
-			return this === 1 ? '' : 's';
+		pluralize: function(str1, str2) {
+			str1 = str1 || '';
+			str2 = str2 || 's';
+			return this === 1 ? str1 : str2;
 		},
 
 		//pprint
