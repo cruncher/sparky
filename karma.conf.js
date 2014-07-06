f6b41964674c9de9b7ed0397489882b83e3f7423
stephband <stephband@googlemail.com>
2014-07-06 09:56:18 +0200

    Adds karma testing

---
+++ karma.conf.js
@@ -0,0 +1,67 @@
+// Karma configuration
+// Generated on Sun Jul 06 2014 04:18:28 GMT+0200 (CEST)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['qunit'],
+
+    // list of files / patterns to load in the browser
+    files: [
+      'src/jquery-2.1.1.js',
+      'src/window.requestanimationframe.js',
+      'src/observe.js',
+      'src/mixin.array.js',
+      'src/mixin.events.js',
+      'src/mixin.storage.js',
+      'src/collection.js',
+      'src/sparky.js',
+      'src/sparky.bind.js',
+      'src/sparky.filters.js',
+
+      'test/*.html',
+      'test/*.js'
+    ],
+
+    // list of files to exclude
+    exclude: [
+      
+    ],
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {},
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+    // web server port
+    port: 9876,
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome', 'Firefox', 'Safari'],
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false
+  });
+};
---
+++ test/test.js
@@ -0,0 +1,72 @@
+var module = (function(QUnit) {
+	var fixture = document.createElement('div');
+	var rcomment = /\s*\/\*([\s\S]*)\*\/\s*/;
+
+	function multiline(fn) {
+		if (typeof fn !== 'function') { throw new TypeError('multiline(fn) expects a function.'); }
+		var match = rcomment.exec(fn.toString());
+		if (!match) { throw new TypeError('Multiline comment missing.'); }
+		return match[1];
+	}
+
+	fixture.id = 'qunit-fixture';
+	document.body.appendChild(fixture);
+
+	return function module(name, fn1, fn2) {
+		QUnit.module(name, {
+			setup: function() {
+				if (fn1) { fixture.innerHTML = multiline(fn1); }
+			}
+		});
+		
+		fn2 && fn2(fixture);
+	}
+})(QUnit);
+
+
+module('Controller', function() {
+/*
+	<div data-ctrl="test-ctrl">{{property}}</div>
+*/
+}, function(fixture) {
+	test("ctrl passed in as fn parameter and {{tag}} replaced with scope property", function() {
+		var node = fixture.querySelector('div');
+	
+		Sparky(node, undefined, function(node, model, sparky) {
+			ok(model === undefined);
+			return { property: 'peas' };
+		});
+	
+		ok(node.innerHTML === 'peas');
+	});
+	
+	test("ctrl found in Sparky.controllers and {{tag}} replaced with scope property", function() {
+		var node = fixture.querySelector('div');
+		
+		Sparky.controllers['test-ctrl'] = function(node, model, sparky) {
+			ok(model === undefined);
+			return { property: 'peas' };
+		};
+		
+		Sparky(node);
+		ok(node.innerHTML === 'peas');
+	});
+});
+
+
+module('Model', function() {
+/*
+	<div data-model="test-model">{{property}}</div>
+*/
+}, function(fixture) {
+	Sparky.data['test-model'] = {
+		property: 'juice'
+	};
+	
+	test("{{tag}} is replaced with model property", function() {
+		var node = fixture.querySelector('div');
+		Sparky(node);
+		ok(node.innerHTML === 'juice');
+	});
+});
+
