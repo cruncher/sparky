a72c0b1f8e8bee3f68d0991e96808a2ef3a8cbb0
stephband <stephband@googlemail.com>
2014-09-14 18:56:26 +0200

    Updates filter tests to match new filter params

--- test/test.filters.js
+++ test/test.filters.js
@@ -1,30 +1,30 @@
 module('Sparky.filters', function(fixture) {
 	test("Sparky.filters.add", function() {
-		ok(Sparky.filters.add.call('5.5', 3.5) === 9);
+		ok(Sparky.filters.add('5.5', 3.5) === 9);
 	});
 
 	test("Sparky.filters.slugify", function() {
-		ok(Sparky.filters.slugify.call('Pardon Me sir') === 'pardon-me-sir');
+		ok(Sparky.filters.slugify('Pardon Me sir') === 'pardon-me-sir');
 	});
 
 	test("Sparky.filters.yesno", function() {
-		ok(Sparky.filters.yesno.call(true, '1', '2') === '1');
-		ok(Sparky.filters.yesno.call({}, '1', '2') === '1');
-		ok(Sparky.filters.yesno.call(false, '1', '2') === '2');
-		ok(Sparky.filters.yesno.call(undefined, '1', '2') === '2');
-		ok(Sparky.filters.yesno.call(null, '1', '2') === '2');
+		ok(Sparky.filters.yesno(true, '1', '2') === '1');
+		ok(Sparky.filters.yesno({}, '1', '2') === '1');
+		ok(Sparky.filters.yesno(false, '1', '2') === '2');
+		ok(Sparky.filters.yesno(undefined, '1', '2') === '2');
+		ok(Sparky.filters.yesno(null, '1', '2') === '2');
 	});
 
 	test("Sparky.filters.prepad", function() {
-		ok(Sparky.filters.prepad.call('barf', '9') === '     barf');
+		ok(Sparky.filters.prepad('barf', '9') === '     barf');
 		// TODO: This is failing - FIX in filter fn!
-		//ok(Sparky.filters.prepad.call('barf', '2') === 'barf');
+		//ok(Sparky.filters.prepad('barf', '2') === 'barf');
 	});
 
 	test("Sparky.filters.postpad", function() {
-		ok(Sparky.filters.postpad.call('barf', '9') === 'barf     ');
-		ok(Sparky.filters.postpad.call('barf', '4') === 'barf');
-		ok(Sparky.filters.postpad.call('barf', '2') === 'ba');
+		ok(Sparky.filters.postpad('barf', '9') === 'barf     ');
+		ok(Sparky.filters.postpad('barf', '4') === 'barf');
+		ok(Sparky.filters.postpad('barf', '2') === 'ba');
 	});
 });
 
