550b60ae0f1853ccaa01b50dd09beb50f8725ac5
stephband <stephband@googlemail.com>
2014-10-07 03:41:06 +0200

    Build

--- js/sparky.js
+++ js/sparky.js
@@ -43,6 +43,25 @@
 	}
 })(window);
 
+// Number.isNaN(n) polyfill
+
+if (!Number.isNaN) {
+	(function(globalIsNaN) {
+		"use strict";
+	
+		Object.defineProperty(Number, 'isNaN', {
+			value: function isNaN(value) {
+				return typeof value === 'number' && globalIsNaN(value);
+			},
+			configurable: true,
+			enumerable: false,
+			writable: true
+		});
+	})(isNaN);
+}
+
+// mixin.array
+
 (function(ns, undefined) {
 	"use strict";
 
@@ -309,14 +328,12 @@
 	};
 })(this);
 
-// Observe and unobserve
-// 
 // observe(obj, [prop], fn)
 // unobserve(obj, [prop], [fn])
 // 
-// Crudely observes object properties for changes by redefining
-// properties of the observable object with setters that fire
-// a callback function whenever the property changes.
+// Observes object properties for changes by redefining
+// properties of the observable object with setters that
+// fire a callback function whenever the property changes.
 
 (function(ns){
 	var slice = Array.prototype.slice,
@@ -439,6 +456,8 @@
 	ns.observe = observe;
 	ns.unobserve = unobserve;
 })(window);
+
+// Collection()
 
 (function(ns, mixin, undefined) {
 	"use strict";
@@ -1404,227 +1423,7 @@
 	});
 })(jQuery, Sparky);
 
-// Sparky.observe()
-// Sparky.unobserve()
-
-(function(Sparky) {
-	"use strict";
-
-	function isAudioParam(object) {
-		return window.AudioParam && window.AudioParam.prototype.isPrototypeOf(object);
-	}
-
-	function Poll(object, property, fn) {
-		var v1 = object[property];
-		var active = true;
-
-		function frame() {
-			var v2 = object[property];
-
-			if (v1 !== v2) {
-				v1 = v2;
-				fn();
-			}
-
-			if (!active) { return; } 
-
-			window.requestAnimationFrame(frame);
-		};
-
-		function cancel() {
-			active = false;
-		}
-
-		window.requestAnimationFrame(frame);
-
-		return cancel;
-	}
-
-	var unpollers = [];
-
-	function poll(object, property, fn) {
-		unpollers.push([object, property, fn, Poll(object, property, fn)]);
-		return object;
-	}
-	
-	function unpoll(object, property, fn) {
-		var n = unpollers.length;
-		var unpoller;
-
-		while (n--) {
-			unpoller = unpollers[n];
-
-			if (object === unpoller[0] && property === unpoller[1] && fn === unpoller[2]) {
-				unpoller[3]();
-				unpollers.splice(n, 1);
-				return object;
-			}
-		}
-
-		return object;
-	}
-
-	(false && Object.observe && window.WeakMap ? function(Sparky) {
-		if (Sparky.debug) { console.log('[Sparky] Ooo. Lucky you, using Object.observe and WeakMap.'); }
-
-		var map = new WeakMap();
-		var names = [];
-		var types = ["add", "update", "delete"];
-
-		function call(fn) {
-			fn(this);
-		}
-
-		function trigger(change) {
-			var properties = this;
-			var name = change.name;
-			var object = change.object;
-
-			// If this property is in the properties being watched, and we
-			// have not already called changes on it, do that now.
-			if (properties[name] && names.indexOf(name) === -1) {
-				names.push(name);
-				fns = properties[name];
-				fns.forEach(call, object);
-			}
-		}
-
-		function triggerAll(changes) {
-			names.length = 0;
-			changes.forEach(trigger, map[object]);
-		}
-
-		function setup(object) {
-			var properties = map[object] = {};
-			Object.observe(object, triggerAll, types);
-			return properties;
-		}
-
-		function teardown(object) {
-			Object.unobserve(object, triggerAll);
-		}
-
-		Sparky.observe = function(object, property, fn) {
-			var properties = map[object] || setup(object);
-			var fns = properties[property] || (properties[property] = []);
-			fns.push(fn);
-		};
-
-		Sparky.unobserve = function(object, property, fn) {
-			if (!map[object]) { return; }
-
-			var properties = map[object];
-			var fns = properties[property];
-
-			if (!fns) { return; }
-
-			var n = fns.length;
-
-			while (n--) { fns.splice(n, 1); }
-
-			if (fns.length === 0) {
-				delete properties[property];
-
-				if (Object.keys[properties].length === 0) {
-					teardown(object);
-				}
-			}
-		};
-	} : function(Sparky) {
-		Sparky.observe = function(object, property, fn) {
-			// AudioParams objects must be polled, as they cannot be reconfigured
-			// to getters/setters, nor can they be Object.observed. And they fail
-			// to do both of those completely silently. So we test the scope to see
-			// if it is an AudioParam and set the observe and unobserve functions
-			// to poll.
-			if (isAudioParam(object)) {
-				return poll(object, property, fn);
-			}
-
-			if (property === 'length') {
-				// Observe length and update the DOM on next
-				// animation frame if it changes.
-				var descriptor = Object.getOwnPropertyDescriptor(object, property);
-	
-				if (!descriptor.get && !descriptor.configurable) {
-					console.warn('[Sparky] Are you trying to observe an array?. Sparky is going to observe it by polling. You may want to use a Sparky.Collection() to avoid this.');
-					return poll(object, property, fn);
-				}
-			}
-
-			return observe(object, property, fn);
-		};
-
-		Sparky.unobserve = function(object, property, fn) {
-			if (isAudioParam(object)) {
-				return unpoll(object, property, fn);
-			}
-
-			if (property === 'length') {
-				var descriptor = Object.getOwnPropertyDescriptor(object, property);
-				if (!descriptor.get && !descriptor.configurable) {
-					return unpoll(object, property, fn);
-				}
-			}
-	
-			return unobserve(object, property, fn);
-		};
-	})(Sparky)
-})(Sparky);
-
-// Sparky.Throttle(fn)
-
-(function() {
-	"use strict";
-
-	function noop() {}
-
-	function Throttle(fn) {
-		var queued, scope, args;
-
-		function update() {
-			queued = false;
-			fn.apply(scope, args);
-		}
-
-		function cancel() {
-			// Don't permit further changes to be queued
-			queue = noop;
-
-			// If there is an update queued apply it now
-			if (queued) { update(); }
-
-			// Make the queued update do nothing
-			fn = noop;
-		}
-
-		function queue() {
-			// Store the latest scope and arguments
-			scope = this;
-			args = arguments;
-
-			// Don't queue update if it's already queued
-			if (queued) { return; }
-
-			// Queue update
-			window.requestAnimationFrame(update);
-			queued = true;
-		}
-
-		function throttle() {
-			queue.apply(this, arguments);
-		}
-
-		throttle.cancel = cancel;
-		update();
-
-		return throttle;
-	}
-
-	Sparky.Throttle = Throttle;
-})(Sparky);
-
-// DOM Binder
+// Sparky.bind
 //
 // Binds data to the DOM. Changes in data are then immediately rendered
 // in the nodes that display that data via template tags such as {{ name }}.
@@ -2114,7 +1913,227 @@
 	Sparky.attributes = attributes;
 })(window.Sparky || require('sparky'));
 
-// Sparky controllers.
+// Sparky.observe()
+// Sparky.unobserve()
+
+(function(Sparky) {
+	"use strict";
+
+	function isAudioParam(object) {
+		return window.AudioParam && window.AudioParam.prototype.isPrototypeOf(object);
+	}
+
+	function Poll(object, property, fn) {
+		var v1 = object[property];
+		var active = true;
+
+		function frame() {
+			var v2 = object[property];
+
+			if (v1 !== v2) {
+				v1 = v2;
+				fn();
+			}
+
+			if (!active) { return; } 
+
+			window.requestAnimationFrame(frame);
+		};
+
+		function cancel() {
+			active = false;
+		}
+
+		window.requestAnimationFrame(frame);
+
+		return cancel;
+	}
+
+	var unpollers = [];
+
+	function poll(object, property, fn) {
+		unpollers.push([object, property, fn, Poll(object, property, fn)]);
+		return object;
+	}
+	
+	function unpoll(object, property, fn) {
+		var n = unpollers.length;
+		var unpoller;
+
+		while (n--) {
+			unpoller = unpollers[n];
+
+			if (object === unpoller[0] && property === unpoller[1] && fn === unpoller[2]) {
+				unpoller[3]();
+				unpollers.splice(n, 1);
+				return object;
+			}
+		}
+
+		return object;
+	}
+
+	(false && Object.observe && window.WeakMap ? function(Sparky) {
+		if (Sparky.debug) { console.log('[Sparky] Ooo. Lucky you, using Object.observe and WeakMap.'); }
+
+		var map = new WeakMap();
+		var names = [];
+		var types = ["add", "update", "delete"];
+
+		function call(fn) {
+			fn(this);
+		}
+
+		function trigger(change) {
+			var properties = this;
+			var name = change.name;
+			var object = change.object;
+
+			// If this property is in the properties being watched, and we
+			// have not already called changes on it, do that now.
+			if (properties[name] && names.indexOf(name) === -1) {
+				names.push(name);
+				fns = properties[name];
+				fns.forEach(call, object);
+			}
+		}
+
+		function triggerAll(changes) {
+			names.length = 0;
+			changes.forEach(trigger, map[object]);
+		}
+
+		function setup(object) {
+			var properties = map[object] = {};
+			Object.observe(object, triggerAll, types);
+			return properties;
+		}
+
+		function teardown(object) {
+			Object.unobserve(object, triggerAll);
+		}
+
+		Sparky.observe = function(object, property, fn) {
+			var properties = map[object] || setup(object);
+			var fns = properties[property] || (properties[property] = []);
+			fns.push(fn);
+		};
+
+		Sparky.unobserve = function(object, property, fn) {
+			if (!map[object]) { return; }
+
+			var properties = map[object];
+			var fns = properties[property];
+
+			if (!fns) { return; }
+
+			var n = fns.length;
+
+			while (n--) { fns.splice(n, 1); }
+
+			if (fns.length === 0) {
+				delete properties[property];
+
+				if (Object.keys[properties].length === 0) {
+					teardown(object);
+				}
+			}
+		};
+	} : function(Sparky) {
+		Sparky.observe = function(object, property, fn) {
+			// AudioParams objects must be polled, as they cannot be reconfigured
+			// to getters/setters, nor can they be Object.observed. And they fail
+			// to do both of those completely silently. So we test the scope to see
+			// if it is an AudioParam and set the observe and unobserve functions
+			// to poll.
+			if (isAudioParam(object)) {
+				return poll(object, property, fn);
+			}
+
+			if (property === 'length') {
+				// Observe length and update the DOM on next
+				// animation frame if it changes.
+				var descriptor = Object.getOwnPropertyDescriptor(object, property);
+	
+				if (!descriptor.get && !descriptor.configurable) {
+					console.warn('[Sparky] Are you trying to observe an array?. Sparky is going to observe it by polling. You may want to use a Sparky.Collection() to avoid this.');
+					return poll(object, property, fn);
+				}
+			}
+
+			return observe(object, property, fn);
+		};
+
+		Sparky.unobserve = function(object, property, fn) {
+			if (isAudioParam(object)) {
+				return unpoll(object, property, fn);
+			}
+
+			if (property === 'length') {
+				var descriptor = Object.getOwnPropertyDescriptor(object, property);
+				if (!descriptor.get && !descriptor.configurable) {
+					return unpoll(object, property, fn);
+				}
+			}
+	
+			return unobserve(object, property, fn);
+		};
+	})(Sparky)
+})(Sparky);
+
+// Sparky.Throttle(fn)
+
+(function() {
+	"use strict";
+
+	function noop() {}
+
+	function Throttle(fn) {
+		var queued, scope, args;
+
+		function update() {
+			queued = false;
+			fn.apply(scope, args);
+		}
+
+		function cancel() {
+			// Don't permit further changes to be queued
+			queue = noop;
+
+			// If there is an update queued apply it now
+			if (queued) { update(); }
+
+			// Make the queued update do nothing
+			fn = noop;
+		}
+
+		function queue() {
+			// Store the latest scope and arguments
+			scope = this;
+			args = arguments;
+
+			// Don't queue update if it's already queued
+			if (queued) { return; }
+
+			// Queue update
+			window.requestAnimationFrame(update);
+			queued = true;
+		}
+
+		function throttle() {
+			queue.apply(this, arguments);
+		}
+
+		throttle.cancel = cancel;
+		update();
+
+		return throttle;
+	}
+
+	Sparky.Throttle = Throttle;
+})(Sparky);
+
+// Sparky.ctrls
 
 (function() {
 	"use strict";
