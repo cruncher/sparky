85423cc8b1fb920b9ee51690a6fd6c7d6399f25c
stephband <stephband@googlemail.com>
2014-11-12 05:18:30 +0100

    No, we dont want that filter

--- src/sparky.filters.js
+++ src/sparky.filters.js
@@ -129,10 +129,6 @@
 			return Number.prototype.toFixed.call(value, n);
 		},
 
-		defined: function(value, str1, str2) {
-			return isDefined(value) ? str1 : str2 ;
-		},
-
 		// .default() can't work, because Sparky does not send undefined or null
 		// values to be filtered. 
 		//'default': function(value) {
