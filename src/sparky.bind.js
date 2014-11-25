732056e92a7116404c772f59081aa0b8587e12e0
stephband <stephband@googlemail.com>
2014-11-25 22:38:02 +0100

    Add comment

--- src/sparky.bind.js
+++ src/sparky.bind.js
@@ -324,6 +324,10 @@
 
 	function bindAttribute(node, attribute, bind, unbind, get, unobservers) {
 		var isSVG = node instanceof SVGElement;
+
+		// Look for data- aliased attributes before attributes. This is
+		// particularly important for the style attribute IE, as it does not
+		// return invalid CSS text content, so Sparky can't read tags in it.
 		var alias = node.getAttribute('data-' + attribute) ;
 		var value = alias ? alias : isSVG ?
 		    		node.getAttributeNS(Sparky.xlink, attribute) || node.getAttribute(attribute) :
