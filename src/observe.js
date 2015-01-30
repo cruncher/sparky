a87d307642f3782eb7fa2e6720884f25aedf5af7
stephband <stephband@googlemail.com>
2015-01-30 19:33:09 +0100

    Better error

--- src/observe.js
+++ src/observe.js
@@ -66,14 +66,14 @@
 			}
 			
 			if (desc.configurable === false) {
-				console.warn('Property \"' + prop + '\" is not observable: configurable === false. Ignoring.');
+				console.warn('Property \"' + prop + '\" is not observable (configurable: false) in object:', obj);
 				return;
 			}
 		}
 
 		replaceProperty(obj, prop, desc, observer, call);
 	}
-	
+
 	function observe(obj, prop, fn) {
 		var args, key;
 
