2678bbb6b3d5f3170b80cb68f7303d658f29a215
stephband <stephband@googlemail.com>
2017-05-02 19:36:11 +0200

    Adds analyse-on-change

--- plugins/sparky.fn.analyse.js
+++ plugins/sparky.fn.analyse.js
@@ -7,15 +7,35 @@
 	var ga      = window.ga || Fn.noop;
 	var rspaces = Fn.rspaces;
 
-	function analyse(category, action, label, value, fields) {
-		ga('send', 'event', category, action, label, value, fields);
+	function analyse(category, action, label, value) {
+		ga('send', 'event', category, action, label, value);
 	}
 
 	Sparky.fn['analyse-on-click'] = function stickToTop(node, scopes) {
 		var property = dom.attribute('data-analyse', node);
+
+		if (!property) {
+			console.warn('Sparky: data-fn="analyse-on-click" requires data-analyse="category action label"');
+			return;
+		}
+
 		var data     = property.split(rspaces);
 
 		node.addEventListener('click', function() {
+			analyse.apply(null, data);
+		});
+	};
+
+	Sparky.fn['analyse-on-change'] = function stickToTop(node, scopes) {
+		var property = dom.attribute('data-analyse', node);
+		var data     = property.split(rspaces);
+
+		if (!property) {
+			console.warn('Sparky: data-fn="analyse-on-change" requires data-analyse="category action label"');
+			return;
+		}
+
+		node.addEventListener('change', function() {
 			analyse.apply(null, data);
 		});
 	};
