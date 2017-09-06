c776f7007689bba6059f0bcf6f25ad146f02181d
stephband <stephband@googlemail.com>
2017-09-06 19:24:20 +0200

    input-handle delegates change events

--- plugins/sparky.fn.range-handle.js
+++ plugins/sparky.fn.range-handle.js
@@ -2,35 +2,68 @@
 	"use strict";
 	
 	var Fn        = window.Fn;
+	var dom       = window.dom;
 
 	var compose   = Fn.compose;
 	var get       = Fn.get;
 	var is        = Fn.is;
 	var normalise = Fn.normalise;
+	var pipe      = Fn.pipe;
+	var throttle  = Fn.throttle;
+
+	var map = {};
+	var stream;
+
+	function callReducer(node, fn) {
+		fn(node);
+		return node;
+	}
+
+	function toNormalisedValue(input) {
+		var min   = input.min && parseFloat(input.min) || 0;
+		var max   = input.max && parseFloat(input.max) || 100;
+		var value = parseFloat(input.value) ;
+		return normalise(min, max, value);
+	}
+
+	function setup() {
+		stream = dom
+		.event('input change', document.documentElement)
+		.map(get('target'))
+		.each(function(node) {
+			var fns = map[node.id];
+			if (!fns) { return; }
+			fns.reduce(callReducer, node);
+		});
+	}
+
+	function add(id, fn) {
+		// If map has no keys, set it up
+		if (Object.keys(map).length === 0) { setup(); }
+		var fns = map[id] || (map[id] = []);
+		fns.push(throttle(compose(fn, toNormalisedValue)));
+	}
+
+	function remove(id, fn) {
+		if (!map[id]) { return; }
+		var fns = map[id];
+		remove(fns, fn);
+		if (fns.length === 0) { delete map[id]; }
+		if (Object.keys(map).length === 0) { teardown(); }
+	}
+
+	function teardown() {
+		stream.stop();
+	}
 
 	Sparky.fn['range-handle'] = function(node, scopes) {
-		var id    = node.getAttribute('for');
-		//var input = document.getElementById(id);
+		var id = node.getAttribute('for');
 
-		//if (!input) {
-		//	throw new Error('Sparky.fn.range: input id="' + id + '" not found in DOM');
-		//}
+		if (!id) {
+			throw new Error('Sparky.fn.range: node has no for attribute', node);
+		}
 
-		var value;
-
-		dom
-		// We have to delegate the event as we cannot be sure that input
-		// (or node) is actually in the DOM yet. Hmmm.
-		.event('input change', document.body)
-		.map(get('target'))
-		.filter(compose(is(id), get('id')))
-		.filter(function dedup(node) { return node.value !== value; })
-		.each(function(input) {
-			var min   = input.min && parseFloat(input.min) || 0;
-			var max   = input.max && parseFloat(input.max) || 100;
-			var value = parseFloat(input.value) ;
-			var ratio = normalise(min, max, value);
-
+		add(id, function(ratio) {
 			node.style.marginLeft = (ratio * 100) + '%';
 			node.style.transform  = 'translate3d(-' + (ratio * 100) + '%, 0, 0)';
 		});
