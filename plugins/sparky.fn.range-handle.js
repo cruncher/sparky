491408cacfadb9978e8e04316dc301762eb6dff1
stephband <stephband@googlemail.com>
2017-09-06 16:45:28 +0200

    Makes range handle delegate

--- plugins/sparky.fn.range-handle.js
+++ plugins/sparky.fn.range-handle.js
@@ -2,28 +2,34 @@
 	"use strict";
 	
 	var Fn        = window.Fn;
-	var normalise = Fn.normalise;
 
-	function getValue(e) {
-		return e.target.value;
-	}
+	var compose   = Fn.compose;
+	var get       = Fn.get;
+	var is        = Fn.is;
+	var normalise = Fn.normalise;
 
 	Sparky.fn['range-handle'] = function(node, scopes) {
 		var id    = node.getAttribute('for');
-		var input = document.getElementById(id);
+		//var input = document.getElementById(id);
 
-		if (!input) {
-			throw new Error('Sparky.fn.range: input id="' + id + '" not found in DOM');
-		}
+		//if (!input) {
+		//	throw new Error('Sparky.fn.range: input id="' + id + '" not found in DOM');
+		//}
+
+		var value;
 
 		dom
-		.event('input change', input)
-		.map(getValue)
-		.dedup()
-		.each(function(value) {
+		// We have to delegate the event as we cannot be sure that input
+		// (or node) is actually in the DOM yet. Hmmm.
+		.event('input change', document.body)
+		.map(get('target'))
+		.filter(compose(is(id), get('id')))
+		.filter(function dedup(node) { return node.value !== value; })
+		.each(function(input) {
 			var min   = input.min && parseFloat(input.min) || 0;
 			var max   = input.max && parseFloat(input.max) || 100;
-			var ratio = normalise(min, max, parseFloat(value));
+			var value = parseFloat(input.value) ;
+			var ratio = normalise(min, max, value);
 
 			node.style.marginLeft = (ratio * 100) + '%';
 			node.style.transform  = 'translate3d(-' + (ratio * 100) + '%, 0, 0)';
