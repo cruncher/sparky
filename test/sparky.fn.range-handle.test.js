08b2682a86ac1c084d2b2bfa2c7bfb02e9893e45
stephband <stephband@googlemail.com>
2017-10-19 00:20:07 +0200

    Range test

--- test/sparky.fn.range-handle.test.js
+++ test/sparky.fn.range-handle.test.js
@@ -10,8 +10,8 @@ group('[data-fn="range-handle"]', function(test, log, fixture) {
 
 <template id="test-ranges">
 	<input type="range" id="range-input" min="2" />
-	<label data-fn="range-handle" class="no-wrap block" for="range-input">label mr</label>
-	<label data-fn="range-handle" class="no-wrap block" for="range-input">label mr</label>
+	<label data-fn="range-handle" class="no-wrap block test-bubble bubble" for="range-input">label mr</label>
+	<label data-fn="range-handle" class="no-wrap block test-bubble bubble" for="range-input">label mr</label>
 </template>
 
 */});
