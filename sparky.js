8286202d540f1388df436a274a82641f2d820ac3
stephband <stephband@googlemail.com>
2018-04-07 17:23:28 +0200

    Filename change

--- sparky.js
+++ sparky.js
@@ -5,7 +5,7 @@ import './js/sparky.js'
 import './js/sparky.fn.js'
 import './js/sparky.fn.each.js'
 import './js/sparky.fn.template.js'
-import './js/sparky.fn.load.js'
+import './js/sparky.fn.import.js'
 import './js/sparky.transforms.js'
 
 export default window.Sparky;
