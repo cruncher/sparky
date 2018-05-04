e13eb5263fcd481cd23cd4d4c064e872bfaf7b20
stephband <stephband@googlemail.com>
2018-05-04 05:54:35 +0200

    No expose by default

--- sparky.js
+++ sparky.js
@@ -3,23 +3,8 @@ if (window.console && window.console.log) {
     console.log('Sparky      - https://github.com/cruncher/sparky');
 }
 
-const DEBUG = true;
-
-import * as fn  from '../fn/fn.js';
-import * as dom from '../dom/dom.js';
-
-if (DEBUG) {
-    window.fn = fn;
-    window.dom = dom;
-}
-
-import Sparky from './js/sparky.js';
-export default Sparky;
+export { default } from './js/sparky.js';
 import './js/sparky.fn.each.js';
 import './js/sparky.fn.template.js';
 import './js/sparky.fn.import.js';
 import './js/sparky.fn.request.js';
-
-if (DEBUG) {
-    window.Sparky = Sparky;
-}
