3c6b80eeff353055fc63a64fd427cd154f36080c
stephband <stephband@googlemail.com>
2019-02-18 00:53:11 +0100

    Moves bindings to config-mount

---
+++ modules/config-mount.js
@@ -0,0 +1,53 @@
+export default {
+    // All
+    default:  { booleans:   ['hidden'], attributes: ['id', 'title', 'style'] },
+
+    // HTML
+    a:        { attributes: ['href'] },
+    button:   { booleans:   ['disabled'] },
+    form:     { attributes: ['method', 'action'] },
+    fieldset: { booleans:   ['disabled'] },
+    img:      { attributes: ['alt']	},
+    input:    {
+        booleans:   ['disabled', 'required'],
+        attributes: ['name'],
+        types: {
+            button:   { attributes: ['value'] },
+            checkbox: { attributes: [], booleans: ['checked'], value: 'checkbox' },
+            date:     { attributes: ['min', 'max', 'step'], value: 'string' },
+            hidden:   { attributes: ['value'] },
+            image:    { attributes: ['src'] },
+            number:   { attributes: ['min', 'max', 'step'], value: 'number' },
+            radio:    { attributes: [], booleans: ['checked'], value: 'radio' },
+            range:    { attributes: ['min', 'max', 'step'], value: 'number' },
+            reset:    { attributes: ['value'] },
+            submit:   { attributes: ['value'] },
+            time:     { attributes: ['min', 'max', 'step'], value: 'string' },
+            default:  { value: 'string' }
+        }
+    },
+    label:    { attributes: ['for'] },
+    meta:     { attributes: ['content'] },
+    meter:    { attributes: ['min', 'max', 'low', 'high', 'value'] },
+    option:   { attributes: ['value'], booleans: ['disabled'] },
+    output:   { attributes: ['for'] },
+    progress: { attributes: ['max', 'value'] },
+    select:   {
+        attributes: ['name'],
+        booleans: ['disabled', 'required'],
+        types: {
+            default: { value: 'string' }
+        }
+    },
+    textarea: { attributes: ['name'], booleans: ['disabled', 'required'], value: 'string' },
+    time:     { attributes: ['datetime'] },
+
+    // SVG
+    svg:      { attributes: ['viewbox'] },
+    g:        { attributes: ['transform'] },
+    path:     { attributes: ['d', 'transform'] },
+    line:     { attributes: ['x1', 'x2', 'y1', 'y2', 'transform'] },
+    rect:     { attributes: ['x', 'y', 'width', 'height', 'rx', 'ry', 'transform'] },
+    text:     { attributes: ['x', 'y', 'dx', 'dy', 'text-anchor', 'transform'] },
+    use:      { attributes: ['href', 'transform'] }
+};
---
+++ modules/config.js
@@ -0,0 +1,8 @@
+import functions from './fn.js';
+
+export default {
+	attributeFn:      'fn',
+	attributeInclude: 'include',
+	attributePrefix:  'sparky-',
+	functions:        functions
+};
