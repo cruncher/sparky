dc174d9068bfdd08788bc2b1b5e2e4ac6ade0ce1
stephband <stephband@googlemail.com>
2019-04-05 15:56:42 +0200

    fn.get stops previous observables. Im not sure this is correct behaviour. Review.

--- modules/fn.get.js
+++ modules/fn.get.js
@@ -1,6 +1,12 @@
-import { Observable } from '../../fn/module.js';
+import { Observable, nothing } from '../../fn/module.js';
 
 export default function get(node, input, params) {
     const path = params[0];
-    return input.chain((object) => Observable(path, object));
+    var observable = nothing;
+
+    return input.chain((object) => {
+        observable.stop();
+        observable = Observable(path, object);
+        return observable;
+    });
 }
