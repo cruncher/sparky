26d01eb11200f8b4236e0640be7e201ba5a20311
stephband <stephband@googlemail.com>
2019-11-29 23:13:14 +0100

    Port prevent:eventtype

--- modules/fn-prevent.js
+++ modules/fn-prevent.js
@@ -1,6 +1,7 @@
 
 import { preventDefault } from '../../dom/module.js';
+import { register } from './register.js';
 
-export default function prevent(node, input, params) {
+register('prevent', function prevent(node, params) {
     node.addEventListener(params[0], preventDefault);
-}
+});
